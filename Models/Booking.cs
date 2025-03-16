9a2d29ee03194630895b5e8d1b680cf42b6a4705
AryanAwasthi138 <aryanawasthi138@gmail.com>
2025-03-16 15:18:33 +0530

    backend

---
+++ Models/Booking.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace TableTennisBooking.Models
+{
+
+    public class Booking
+    {
+        [Key]
+        public int BookingId { get; set; }
+        public int SlotId { get; set; }
+        public int TableId { get; set; }
+
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+
+        public Slot Slot { get; set; }
+        public string UserName { get; set; }
+        public DateTime BookingDate { get; set; }
+    }
+}
+
